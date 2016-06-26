5f44991811fab4c4ebe2509294dfee0e24d75693
Christopher Lopez <chrisa_lopez@yahoo.com>
2016-06-26 10:57:47 -0700

    merge

---
+++ project/project0/script.js
@@ -0,0 +1,9 @@
+// $('#field1').click( function() {
+//     if(f1 != "X" && f1 != "O") {
+//         $(this).css('background-image', 'url(http://i61.tinypic.com/faz9g3.png)');
+//         f1 = "X";
+//         count++;
+//         checkVictory();
+//         randomize();
+//     }
+// });
